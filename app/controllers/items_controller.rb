4a231920b76f5376af7536c50445a8df43af263f
Karl Xavier <kxdc1025@yahoo.com>
2016-04-27 06:10:51 -0400

    added partial forms and controller methods

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
 	before_action :authenticate_user!, except: [:index, :show]
+	before_action :set_item, only: [:show, :edit, :update, :destroy]
+	before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
 
 	def index
 		@items = Item.all
@@ -8,4 +10,21 @@ class ItemsController < ApplicationController
 	def new
 		@item = Item.new
 	end
+
+	def create
+		
+	end
+
+	private
+		def set_item
+			@item = Item.find(params[:id])
+		end
+
+		def item_params
+			params.require(:items).permit(:name, :description, :original_price, :supplier_id, :item_type_id)
+		end
+
+	    def set_s3_direct_post
+	      @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+	    end
 end
